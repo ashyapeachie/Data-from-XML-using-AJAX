107fedf54c6e64b8c8687c8cf7ba72e8452b9d3f
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-12-10 22:54:54 -0500

    window & loadXML function

--- script.js
+++ script.js
@@ -6,5 +6,23 @@
 
 let xmlData = null;
 
-
+window.onload = function () {
+  loadXML();
+};
 // load XML on the loading page
+
+function loadXML() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "class_catalog.xml", true);
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      xmlData = xhr.responseXML;
+      console.log("XML Loaded!");
+    } else {
+      console.error("Failed to load XML.");
+    }
+  };
+
+  xhr.send();
+}
