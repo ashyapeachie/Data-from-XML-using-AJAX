76caef9d9314090306f75d7c7b3f89deee6e0e2b
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-12-10 23:55:50 -0500

    last minute changes & adding to json file

--- server.js
+++ server.js
@@ -14,11 +14,6 @@ const path = require("path");
 /* files from directory */
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "Final_Project_AshyaHamer.html"));
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
